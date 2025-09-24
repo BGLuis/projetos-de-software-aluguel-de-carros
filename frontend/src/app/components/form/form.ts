2fff4a7d74d88d63e346f7ad6fa0c4d776ac4ef1
jaoppb <joaopedroperes06@gmail.com>
2025-09-24 17:42:26 -0300

    fix(front): validating

--- frontend/src/app/components/form/form.ts
+++ frontend/src/app/components/form/form.ts
@@ -1,8 +1,9 @@
 import { Component, computed, Input, Signal } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormGroup, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { LucideAngularModule } from 'lucide-angular';
 
 export type StepInfo = {
+	formGroup: FormGroup;
 	button: {
 		text: string;
 		icon: string;
@@ -11,26 +12,35 @@ export type StepInfo = {
 
 @Component({
 	selector: 'app-form',
-	imports: [FormsModule, LucideAngularModule],
+	imports: [ReactiveFormsModule, LucideAngularModule],
 	templateUrl: './form.html',
 	styleUrl: './form.scss',
 })
 export class Form {
 	@Input({ required: true }) onSubmit!: (form: any) => void;
 	@Input({ required: true }) stepInfo!: Signal<StepInfo[]>;
-	@Input({ required: true }) title!: string;
 	currentStepIndex = 0;
 	endStepIndex = computed(() => this.stepInfo().length - 1);
 
-	_onSubmit(f: NgForm) {
-		if (!f.valid) return;
+	_onSubmit() {
+		if (this.currentFormGroup.invalid) {
+			this.currentFormGroup.markAllAsTouched();
+			return;
+		}
 
 		if (this.currentStepIndex < this.endStepIndex()) {
 			this.currentStepIndex++;
+		} else {
+			const combinedValue = this.stepInfo().reduce(
+				(acc, step) => ({ ...acc, ...step.formGroup.value }),
+				{},
+			);
+			this.onSubmit(combinedValue);
 		}
+	}
 
-		if (this.currentStepIndex === this.endStepIndex())
-			this.onSubmit(f.value);
+	get currentFormGroup(): FormGroup {
+		return this.stepInfo()[this.currentStepIndex].formGroup;
 	}
 
 	previousStep() {
--- frontend/src/app/pages/auth/sign-up/sign-up.ts
+++ frontend/src/app/pages/auth/sign-up/sign-up.ts
@@ -1,14 +1,18 @@
-import { Component, Signal, signal } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { HeaderComponent } from '#components/header/header';
 import { CarBackground } from '#components/car-background/car-background';
 import { Step } from '#components/form/step/step';
 import { Form, StepInfo } from '#components/form/form';
 import { Steps } from '#components/form/steps/steps';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Cities, City, State, States } from '#app/utils';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
 	selector: 'app-sign-up',
 	imports: [
+		ReactiveFormsModule,
 		HeaderComponent,
 		LucideAngularModule,
 		CarBackground,
@@ -20,10 +24,93 @@ import { Steps } from '#components/form/steps/steps';
 	styleUrl: './sign-up.scss',
 })
 export class SignUp {
-	stepInfo: Signal<StepInfo[]> = signal([
-		{ button: { text: 'Próximo Passo', icon: 'arrow-right' } },
-		{ button: { text: 'Concluir Cadastro', icon: 'check' } },
-	]);
+	states = States;
+	stepInfo: Signal<StepInfo[]>;
+	selectedState: Signal<State | null | undefined>;
+	cities: Signal<City[]>;
+
+	constructor(private readonly formBuilder: FormBuilder) {
+		this.stepInfo = signal([
+			{
+				formGroup: this.formBuilder.group({
+					personal: this.formBuilder.group({
+						fullName: [
+							'',
+							[Validators.required, Validators.minLength(3)],
+						],
+						telephone: [
+							'',
+							[Validators.required, Validators.minLength(10)],
+						],
+						email: ['', [Validators.required, Validators.email]],
+						rg: [
+							'',
+							[Validators.required, Validators.minLength(9)],
+						],
+						cpf: [
+							'',
+							[
+								Validators.required,
+								Validators.minLength(11),
+								Validators.maxLength(11),
+							],
+						],
+					}),
+					location: this.formBuilder.group({
+						cep: [
+							'',
+							[
+								Validators.required,
+								Validators.minLength(8),
+								Validators.maxLength(8),
+							],
+						],
+						state: [null, [Validators.required]],
+						city: [null, [Validators.required]],
+						neighborhood: ['', [Validators.required]],
+						street: ['', [Validators.required]],
+						number: ['', [Validators.required]],
+						complement: [''],
+					}),
+				}),
+				button: { text: 'Próximo Passo', icon: 'arrow-right' },
+			},
+			{
+				formGroup: this.formBuilder.group({
+					profissional: this.formBuilder.group({
+						profession: ['', [Validators.required]],
+						company: ['', [Validators.required]],
+						monthlyIncome: [
+							0,
+							[Validators.required, Validators.min(100)],
+						],
+					}),
+					password: this.formBuilder.group({
+						password: ['', [Validators.required]],
+						confirmPassword: ['', [Validators.required]],
+					}),
+				}),
+				button: { text: 'Concluir Cadastro', icon: 'check' },
+			},
+		]);
+
+		const stateControl = this.stepInfo()
+			.at(0)
+			?.formGroup.get('location.state');
+
+		if (!stateControl) {
+			this.selectedState = signal(null);
+		} else {
+			this.selectedState = toSignal(stateControl.valueChanges, {
+				initialValue: stateControl.value,
+			});
+		}
+
+		this.cities = computed(() => {
+			const state = this.selectedState();
+			return state ? Cities[state] : [];
+		});
+	}
 
 	onSubmit(form: any) {
 		console.log(form);
