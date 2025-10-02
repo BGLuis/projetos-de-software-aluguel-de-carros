5f32e14f4dc0d3d57beefbc9294db17dc3dcca9b
jaoppb <joaopedroperes06@gmail.com>
2025-10-01 22:02:52 -0300

    feat(front): search

--- frontend/src/app/components/search-bar/search-bar.ts
+++ frontend/src/app/components/search-bar/search-bar.ts
@@ -1,9 +1,16 @@
 import { Component, Input } from '@angular/core';
+import {
+	FormBuilder,
+	FormSubmittedEvent,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
 	selector: 'app-search-bar',
-	imports: [LucideAngularModule],
+	imports: [ReactiveFormsModule, LucideAngularModule],
 	templateUrl: './search-bar.html',
 	styleUrl: './search-bar.scss',
 })
@@ -12,9 +19,37 @@ export class SearchBar {
 
 	totalFields = 4 + (this.hasNextButton ? 1 : 0);
 
+	formGroup;
+
+	constructor(
+		private readonly formBuilder: FormBuilder,
+		private readonly router: Router,
+	) {
+		this.formGroup = this.formBuilder.group({
+			pickupLocation: ['', [Validators.required]],
+			pickupDate: ['', [Validators.required]],
+			returnLocation: ['', [Validators.required]],
+			returnDate: ['', [Validators.required]],
+		});
+	}
+
 	getWidthStyle() {
 		return `width: calc((100% - ${this.totalFields * 16}px) / ${
 			this.totalFields
 		})`;
+	}
+
+	onSubmit() {
+		if (this.formGroup.valid) {
+			const formData = this.formGroup.value;
+			this.router.navigate(['/rent'], {
+				queryParams: {
+					pickupLocation: formData.pickupLocation,
+					pickupDate: formData.pickupDate,
+					returnLocation: formData.returnLocation,
+					returnDate: formData.returnDate,
+				},
+			});
+		}
 	}
 }
