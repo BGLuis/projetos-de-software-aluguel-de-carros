e4a900f58f8968be2c8658d6d3aadb48718c2b93
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-10-02 12:26:39 -0300

    feat: skeleton rent page

--- frontend/src/app/pages/rent/rent.ts
+++ frontend/src/app/pages/rent/rent.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { TopPage } from '#app/components/top-page/top-page';
+import { StatusProgress } from '#app/components/status-progress/status-progress';
+import { Icons } from '#app/components/icons/icons';
+import { Car, CarCard } from '#app/components/car-card/car-card';
 
 const choices = [
 	{
@@ -35,10 +38,47 @@ const choices = [
 
 @Component({
 	selector: 'app-rent',
-	imports: [TopPage],
+	imports: [TopPage, StatusProgress, CarCard, Icons],
 	templateUrl: './rent.html',
 	styleUrl: './rent.scss',
 })
 export class Rent {
 	choices = choices;
+	statuses = [
+		{
+			label: 'Local e Data',
+			value: true,
+			editOption: {
+				label: 'Editar',
+				action: () => this.editLocalEData()
+			}
+		},
+		{
+			label: 'Escolha do Carro',
+			value: false,
+		},
+		{
+			label: 'Tarifas e Adicionais',
+			value: false,
+		},
+		{
+			label: 'Dados Cadastrais',
+			value: false,
+		}
+	];
+
+	carsEX: Car = {
+		imageURL: 'cars/bmw-m4.png',
+		name: 'BMW M4',
+		backgroundName: 'M4',
+		pricePerDay: 2000,
+		gasType: 'flex',
+		gearType: 'automatic',
+		seats: 4,
+	};
+
+	editLocalEData() {
+		// Lógica para editar local e data
+		console.log('Editando Local e Data');
+	}
 }
